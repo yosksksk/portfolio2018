dc8310bc61a89f21d55598f00fe8b1050fbe19d7
Yoshiki Nishioka <yoshiki.nishioka.kobe@gmail.com>
2018-09-12 04:57:00 +0900

    second commit

--- js/svg.js
+++ js/svg.js
@@ -1,11 +1,11 @@
 var st0 = document.querySelectorAll('.st0');
 var anime = function(){
-    new Vivus('svg-animation', {type: 'scenario-sync',duration: 20, forceRender: false ,animTimingFunction:Vivus.EASE},function(){
+    new Vivus('svg-animation', {type: 'scenario-sync',duration: 5, forceRender: false ,animTimingFunction:Vivus.EASE},function(){
     setTimeout(function(){  
         for(var i =0;i < st0.length; i ++)
             st0[i].removeAttribute('style');
         anime();
-    },10000)
+    },3000)
     });
 }
 anime();
