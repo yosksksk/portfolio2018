310079c90edb56c2dca25eee78a63ddbd4df779e
Yoshiki Nishioka <yoshiki.nishioka.kobe@gmail.com>
2018-12-05 23:32:37 +0900

    console message

---
+++ js/message.js
@@ -0,0 +1,4 @@
+var style1="color:#E39261; font-size: 15px; font-weight: bold;"
+var style2="color: Blue; font-size: 15px; font-weight: bold;"
+console.log("%cエンジニア案件からカメラマン案件、イベント企画など幅広く依頼Wanted!" , style1);
+console.log("%c https://twitter.com/uuknk_yn" , style2);
